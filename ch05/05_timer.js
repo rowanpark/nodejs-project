3c7278d236550a932d18692e8e67aed21041e145
rowanpark <rowan.park.83@gmail.com>
2023-04-19 17:26:33 +0900

    Study: Study Node.js

---
+++ ch05/05_timer.js
@@ -0,0 +1,21 @@
+const timeout = setTimeout(() => {
+  console.log("1초 후에 실행됩니다.");
+}, 1000);
+
+var count = 1;
+const interval = setInterval(() => {
+  console.log("%d: 1초 마다 실행됩니다.", count);
+  count++;
+}, 1000);
+
+const immediate = setImmediate(() => {
+  console.log(
+    "setImmediate() 함수 호출 뒤에 오는 모든 코드를 먼저 실행하고 바로 다음에 실행합니다."
+  );
+});
+
+console.log("setImmediate보다 먼저 실행됩니다.");
+
+setTimeout(() => {
+  clearInterval(interval); // 변수명이 interval인 1초마다 콘솔창에 출력하기 위해 정의한 setInterval() 함수를 종료합니다.
+}, 5000); // 4초까지만 출력될 때도 있다.
